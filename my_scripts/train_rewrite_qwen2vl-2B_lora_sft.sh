b3ffca56c7cc65e5311971801d3ae03f0a82174a
Jinghong Chen <erichen0615@gmail.com>
2024-11-04 14:12:21 +0000

    2B norag answer lora sft

--- my_scripts/train_rewrite_qwen2vl-2B_lora_sft.sh
+++ my_scripts/train_rewrite_qwen2vl-2B_lora_sft.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-llamafactory-cli train my_configs/qwen2vl-2B_lora_sft.yaml
-# llamafactory-cli train my_configs/rewrite_qwen2vl-2B_lora_sft.yaml
+# llamafactory-cli train my_configs/qwen2vl-2B_lora_sft.yaml
+llamafactory-cli train my_configs/rewrite_qwen2vl-2B_lora_sft.yaml
