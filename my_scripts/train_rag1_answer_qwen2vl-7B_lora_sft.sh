52fe42f1606a80c052159ef02a71c8eef918ff45
J. Chen <jc2124@login-p-2.data.cluster>
2024-11-07 15:56:39 +0000

    add rag1 for 7B lora

---
+++ my_scripts/train_rag1_answer_qwen2vl-7B_lora_sft.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J train_rag1_answer_qwen2vl-7B_lora_sft
+#SBATCH -A BYRNE-SL2-GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=36:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+#SBATCH -p ampere
+export WANDB_RUN_GROUP="HPC"
+
+which python
+
+llamafactory-cli train my_configs/rag1_answer_qwen2vl-7B_lora_sft.yaml
