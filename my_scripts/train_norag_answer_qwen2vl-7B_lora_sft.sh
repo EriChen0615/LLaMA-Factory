485b2f8a5bcf3291cbafb3d949b62f6b98694ac4
Jinghong Chen <erichen0615@gmail.com>
2024-12-03 14:45:04 +0000

    rag1answer 2b

---
+++ my_scripts/train_norag_answer_qwen2vl-7B_lora_sft.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+llamafactory-cli train my_configs/norag_answer_qwen2vl-7B_lora_sft.yaml
---
+++ my_scripts/train_rag1_answer_qwen2vl-2B_lora_sft.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+llamafactory-cli train my_configs/rag1_answer_qwen2vl-2B_lora_sft.yaml
