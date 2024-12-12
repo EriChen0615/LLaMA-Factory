68a737c13c8cc6a4a1d7215af7b681a86990b492
J. Chen <jc2124@gpu-q-13.data.cluster>
2024-12-12 16:15:40 +0000

    add infoseek rag1 and reranker trainining

---
+++ my_scripts/infoseek/train_rag1_answer_qwen2vl-2B_lora_sft.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J train_infoseek_rag1_answer_qwen2vl-2B_lora_sft
+#SBATCH -A BYRNE-SL2-GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+#SBATCH -p ampere
+llamafactory-cli train my_configs/infoseek/rag1_answer_qwen2vl-2B_lora_sft.yaml
