4587904c29c75ff4341a1c4ec748e078efee819d
J. Chen <jc2124@gpu-q-2.data.cluster>
2024-12-12 14:29:55 +0000

    add rag5 self-generated data for okvqa. add merge lora script

--- my_scripts/okvqa/train_from-norag_rag1_answer_qwen2vl-2B_lora_sft copy.sh
+++ my_scripts/okvqa/train_from-norag_rag1_answer_qwen2vl-2B_lora_sft copy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-#SBATCH -J train_okvqa-doc1-verify_qwen2vl-2B_lora_sft
+#SBATCH -J train_okvqa_from-norag_rag1_answer_qwen2vl-2B_lora_sft
 #SBATCH -A BYRNE-SL2-GPU
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=32:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #! Uncomment this to prevent the job from being requeued (e.g. if
 #! interrupted by node failure or system downtime):
 ##SBATCH --no-requeue
 #SBATCH -p ampere
-export WANDB_RUN_GROUP="HPC"
-
-which python
-
-llamafactory-cli train my_configs/okvqa/doc1_verify_qwen2vl-2B_lora_sft.yaml
+llamafactory-cli train my_configs/okvqa/from-norag_rag1_answer_qwen2vl-2B_lora_sft.yaml
--- my_scripts/okvqa/train_rag5_answer_sft_qwen2vl_lora_sft.sh
+++ my_scripts/okvqa/train_rag5_answer_sft_qwen2vl_lora_sft.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -J train_okvqa-doc1-verify_qwen2vl-2B_lora_sft
+#SBATCH -J okvqa_train_rag5_answer_sft_qwen2vl_lora_sft
 #SBATCH -A BYRNE-SL2-GPU
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=32:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #! Uncomment this to prevent the job from being requeued (e.g. if
 #! interrupted by node failure or system downtime):
@@ -14,4 +14,4 @@ export WANDB_RUN_GROUP="HPC"
 
 which python
 
-llamafactory-cli train my_configs/okvqa/doc1_verify_qwen2vl-2B_lora_sft.yaml
+llamafactory-cli train my_configs/okvqa/rag5_answer_sft_qwen2vl-2B_lora_sft.yaml
