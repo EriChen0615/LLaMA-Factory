6b91f26e8ce9f9c383080cbea10ef0a61d821f95
J. Chen <jc2124@gpu-q-31.data.cluster>
2025-10-24 11:41:14 +0100

    ckpt

--- my_scripts/evqa/beft/train_beft[K=2*]-prior=s1h4_qwen2vl-2B_lora.sh
+++ my_scripts/evqa/beft/train_beft[K=2*]-prior=s1h4_qwen2vl-2B_lora.sh
@@ -2,7 +2,7 @@
 #SBATCH -A BYRNE-SL3-GPU
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --time=12:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #! Uncomment this to prevent the job from being requeued (e.g. if
@@ -13,4 +13,4 @@ export WANDB_RUN_GROUP="HPC-PPL"
 
 which python
 
-llamafactory-cli train my_configs/evqa/beft/beft[K=4*]-prior=l0h4_qwen2vl-2B_lora.yaml
+llamafactory-cli train my_configs/evqa/beft/beft[K=2*]-prior=s1h4_qwen2vl-2B_lora.yaml
